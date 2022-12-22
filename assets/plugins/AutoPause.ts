f442af20acc03641692b8a2b2d87ec9dc59d14a3
Juliet Garcia <julietgar.duque@gmail.com>
2022-12-22 09:40:34 -0300

    plugins added mediaplayer

---
+++ assets/plugins/AutoPause.ts
@@ -0,0 +1,47 @@
+import MediaPlayer from '../MediaPlayer';
+
+class AutoPause {
+    private threshold: number;
+    player: MediaPlayer;
+
+    constructor() {
+        this.threshold = 0.30;
+        this.handlerIntersection = this.handlerIntersection.bind(this);
+        this.handleVisibilityChange = this.handleVisibilityChange.bind(this);
+    }
+
+    run(player) {
+        this.player = player;
+
+        const observer = new IntersectionObserver(this.handlerIntersection, {
+            threshold: this.threshold
+        });
+
+        observer.observe(this.player.media);
+
+        document.addEventListener("visibilitychange", this.handleVisibilityChange);
+    }
+
+    private handlerIntersection(entries: IntersectionObserverEntry[]) {
+        const entry = entries[0];
+        let isVisible = entry.intersectionRatio >= this.threshold;
+
+        if(isVisible) {
+            this.player.play();
+        } else {
+            this.player.pause();
+        }
+    }
+
+    private handleVisibilityChange() {
+        const isVisible = document.visibilityState === "visible";
+
+        if(isVisible) {
+            this.player.play();
+        } else {
+            this.player.pause();
+        }
+    }
+}
+
+export default AutoPause;
